14f1752d53fb81320679a11f06271b14e5eab61f
asmasultana07 <ab.asma1084@gmail.com>
2025-08-18 02:18:34 +0600

    updated work

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* Typing Animation */
+
+var typed = new Typed(".typing",{
+    strings:["", "Frontend Developer", "Web Designer", "Web Developer", "AI/ML Engineer", "Researcher"],
+    typeSpeed: 150,
+    BackSpeed: 60,
+    loop: true
+})
