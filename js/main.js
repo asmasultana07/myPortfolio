b0054ed78b942705b46ec3ae40328e0995d1689f
asmasultana07 <ab.asma1084@gmail.com>
2025-08-19 07:27:28 +0600

    about done

--- js/main.js
+++ js/main.js
@@ -5,10 +5,32 @@ const menuBtn = document.getElementById('menu-btn');
 const mobileMenu = document.getElementById('mobile-menu');
 const closeBtn = document.getElementById('close-btn');
 
+const tabs = document.querySelectorAll(".tab-btn");
+const contents = document.querySelectorAll(".tab-content");
+
 menuBtn.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden');
 });
 
 closeBtn.addEventListener('click', () => {
   mobileMenu.classList.add('translate-x-full'); // hide menu
+});
+
+// about
+// tab details visible
+
+// const tabs = document.querySelectorAll(".tab-btn");
+// const contents = document.querySelectorAll(".tab-content");
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", (e) => {
+    e.preventDefault(); 
+    
+    tabs.forEach(btn => btn.classList.remove("border-b-2", "border-blue-300"));
+    contents.forEach(content => content.classList.add("hidden"));
+
+    // Active tab highlight 
+    tab.classList.add("border-b-2", "border-blue-300");
+    document.getElementById(tab.dataset.tab).classList.remove("hidden");
+  });
 });
